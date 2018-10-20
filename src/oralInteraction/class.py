be40116d2dbd9aa20ef72968cb43bfd06d13569e
JuanDavidG1997 <jd.garciah@uniandes.edu.co>
2018-10-19 19:52:21 -0500

    Maquina de estados modificados

--- src/oralInteraction/class.py
+++ src/oralInteraction/class.py
@@ -1,11 +1,19 @@
 from OralInteraction import OralInteraction
+from Cliente import Cliente
 DictionaryStates = {'start' : 1, 'introduction' : 2, 'waitingPerson' : 3,'requestName':4, 'listeningName':5, 'verifyName' : 6, 'requestDrink' : 7, 'linsteningDrink':8,...
-... 'verifyDrink':9, 'requestNewPerson':10, 'waitingNewPerson':11, 'exit':12, 'apology' : 13}
+... 'verifyDrink':9, 'requestPerson':10, 'waitingNewPerson':11, 'exit':12, 'apology' : 13, 'listeningNewPerson' : 14, 'verifyNewPerson' : 15...
+... 'affirmationNewPerson' : 16}
 
 state = start
 missunderstoodName = False
 missunderstoodDrink = False
+missunderstoodNewPerson = False
+name = ""
+drink = ""
+newPerson = ""
 OralInteraction = OralInteraction()
+clientCount = 0
+clients = []
 
 while 1
 	print(state)
@@ -18,54 +26,85 @@ while 1
 		
 	if state == waitingPerson:
 		input("Press Enter to state requestName")
-		state = requestName		
+		state = requestName
 
 	if state == requestName:
 		OralInteraction.askForName()
 		state = listeningName
 		
 	if state == listeningName:
-		self.name = OralInteraction.listeningName()
-		state = verifyName
+		name = OralInteraction.listeningName(OralInteraction.capturarAudio())
+		if name == ""
+			missunderstoodName = True
+			state = apology
+		else:
+			state = verifyName
 		
 	if state == verifyName:
-		missunderstoodName = self.verifyName(name)
+		missunderstoodName = OralInteraction.verifyName(name)
 		if missunderstoodName == False
 			state = requestDrink
+			newCliente = Cliente(name,"","no esta listo")
 		else:
 			state = apology
 
 	if state == requestDrink:
-		OralInteraction.solicitarPedido()
+		OralInteraction.askForDrink()
 		state = linsteningDrink
 
 	if state == linsteningDrink:
-		OralInteraction.askForDrink()
-		state = verifyDrink
+		drink = OralInteraction.listeningDrink(OralInteraction.capturarAudio())
+		if drink == ""
+			missunderstoodDrink = True
+			state = apology
+		else:
+			state = verifyDrink
 		
 	if state == verifyDrink:
-		missunderstoodDrink = OralInteraction.verifyDrink(name)
+		missunderstoodDrink = OralInteraction.verifyDrink(drink)
 		if missunderstoodDrink == False
-			state = requestDrink
+			state = requestNewPerson
+			newCliente.cambiarPedido(drink)
 		else:
 			state = apology
-		
-	if state == requestNewPerson:
-		OralInteraction.requestNewPerson()
-		state = waitingPerson
 
-	if state == waitingNewPerson:
-		input("Press Enter to state waitingPerson")
-		
+	if state == requestNewPerson:
+		OralInteraction.askForNewPerson()
+		state = listeningNewPerson
+
+	if state == listeningNewPerson
+		newPerson = OralInteraction.listeningNewPerson(OralInteraction.capturarAudio())
+		if newPerson == ""
+			missunderstoodNewPerson = True
+			state = apology
+		else:
+			state = verifyNewPerson
+
+	if state == verifyNewPerson
+		missunderstoodNewPerson = OralInteraction.verifyNewPerson(newPerson)
+		if missunderstoodNewPerson == False:
+			state = affirmationNewPerson
+		else:
+			state = apology
+
+	if state == affirmationNewPerson
+		newPersonBoolean = OralInteraction.affirmationCheck(newPerson)
+		if newPersonBoolean == True
+			state = waitingPerson
+		else:
+			state = exit
+
 	if state == exit:
 		OralInteraction.exit()
 
-	if state == apology
+	if state == apology:
+		OralInteraction.apologize()
 		if missunderstoodName == True
-			OralInteraction.apologizeName()
 			missunderstoodName = False
-			state = askForName
-		elif missunderstoodDrink == True:
-			OralInteraction.apologizeDrink()
+			state = requestName
+		elif missunderstoodDrink == True
 			missunderstoodDrink =False
-			state = askForDrink
+			state = requestDrink
+		elif missunderstoodNewPerson == True
+			missunderstoodNewPerson = False
+			state = requestNewPerson
